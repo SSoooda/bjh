868f170d85c5e09e070b7ee7f3cc0324adc971fc
ssoooda <ssoooda@nate.com>
2022-11-03 14:10:54 +0900

    시간과 관련된 테스트 지움

--- src/test/java/com/bjh/webservice/domain/PostsRepositoryTest.java
+++ src/test/java/com/bjh/webservice/domain/PostsRepositoryTest.java
@@ -2,6 +2,7 @@ package com.bjh.webservice.domain;
 
 import com.bjh.webservice.domain.posts.Posts;
 import com.bjh.webservice.domain.posts.PostsRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,11 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.time.LocalDateTime;
 import java.util.List;
-
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
 
 
 @RunWith(SpringRunner.class)
@@ -39,27 +36,10 @@ public class PostsRepositoryTest {
         List<Posts> postsList = postsRepository.findAll();
 
         Posts posts = postsList.get(0);
-        assertThat(posts.getTitle(), is("테스트 게시글"));
-        assertThat(posts.getContent(), is("테스트 본문"));
+        Assertions.assertThat(posts.getTitle()).isEqualTo("테스트 게시글");
+        Assertions.assertThat(posts.getContent()).isEqualTo("테스트 본문");
     }
 
-    @Test
-    public void BaseTimeEntity_등록(){
-        //given
-        LocalDateTime now = LocalDateTime.now();
-        postsRepository.save(Posts.builder()
-                .title("테스트 게시글")
-                .content("테스트 본문")
-                .author("ssoooooda@gmail.com")
-                .build());
-        //when
-        List<Posts> postsList = postsRepository.findAll();
 
-        //then
-        Posts posts = postsList.get(0);
-        assertThat(posts.getCreatedDate(), is(now));
-        assertThat(posts.getModifiedDate(), is(now));
-
-    }
 
 }
