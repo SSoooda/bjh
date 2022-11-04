e83c249286f9558b3d3c90da3978eb803103971e
ssoooda <ssoooda@nate.com>
2022-11-04 18:32:02 +0900

    포트분할

--- src/main/java/com/bjh/webservice/web/controller/WebRestController.java
+++ src/main/java/com/bjh/webservice/web/controller/WebRestController.java
@@ -3,16 +3,20 @@ package com.bjh.webservice.web.controller;
 import com.bjh.webservice.domain.posts.PostsRepository;
 import com.bjh.webservice.web.DTO.PostsSaveRequestDto;
 import lombok.AllArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
 
 @RestController
 @AllArgsConstructor
 public class WebRestController {
 
     private PostsRepository postsRepository;
+    private Environment env;
 
     @PostMapping("/posts")
     public boolean savePosts(@RequestBody PostsSaveRequestDto dto){
@@ -24,4 +28,12 @@ public class WebRestController {
     public String hello() {
         return "HelloWorld";
     }
+
+    @GetMapping("/profile")
+    public String getProfile () {
+        return Arrays.stream(env.getActiveProfiles())
+                .findFirst()
+                .orElse("");
+    }
+
 }
